872cb8867ff7f87f5384f16f0578cc4e04419407
Shymurskyy <shymurskyy@interia.pl>
2024-06-24 22:22:15 +0200

    First model commit

---
+++ weddingApp/weddingApp/Model/Entities/WeddingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using weddingApp.Model.Enum;
+
+namespace weddingApp.Model.Entities
+{
+    public class WeddingService
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ServiceType ServiceType { get; set; }
+        public string ContactInfo { get; set; } 
+    }
+}
