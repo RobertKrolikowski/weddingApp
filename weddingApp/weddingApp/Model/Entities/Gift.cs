b905f6d91fcd24768195afccc2caca2606e576be
Shymurskyy <shymurskyy@interia.pl>
2024-06-24 22:45:46 +0200

    Gift model

---
+++ weddingApp/weddingApp/Model/Entities/Gift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weddingApp.Model.Entities
+{
+    public class Gift
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsPurchased { get; set; }
+        public int CoupleId { get; set; }
+        public Couple Couple { get; set; }
+    }
+
+}
