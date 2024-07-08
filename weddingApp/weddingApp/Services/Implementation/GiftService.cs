029a66b8f28ce8199dbeedc8dbf9b1220170e46d
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2024-07-08 23:19:53 +0200

    Gift controller

--- weddingApp/weddingApp/Services/Implementation/GiftService.cs
+++ weddingApp/weddingApp/Services/Implementation/GiftService.cs
@@ -14,10 +14,43 @@ namespace weddingApp.Services.Implementation
             _configuration = configuration;
             _db = db;
         }
+
         public async Task<IEnumerable<Gift>> GetAllGifts()
         {
-            var gifts = _db.Gifts.ToListAsync();
+            Task<List<Gift>>? gifts = _db.Gifts.ToListAsync();
             return await gifts;
+        }
+        public async Task<Gift> GetGiftById(int id)
+        {
+            Task<Gift?>? gift = _db.Gifts.FirstOrDefaultAsync(x => x.Id == id);
+            return await gift;
+        }
+        public async Task<Gift> CreateGift(Gift gift)
+        {
+            _db.Gifts.Add(gift);
+            await _db.SaveChangesAsync();
+            return gift;
+        }
+
+        public async Task<Gift> UpdateGift(Gift gift)
+        {
+            var existingGift = await _db.Gifts.FindAsync(gift.Id);
+            if (existingGift == null)
+                throw new ArgumentException($"Gift with id {gift.Id} not found");
+
+            existingGift.Description = gift.Description;
+            existingGift.Price = gift.Price;
+            existingGift.IsPurchased = gift.IsPurchased;
+            existingGift.Thing = gift.Thing;
+            _db.Update(existingGift);
+            await _db.SaveChangesAsync();
+            return existingGift;
+        }
+        public async Task<Gift> DeleteGift(Gift gift)
+        {
+            _db.Gifts.Remove(gift);
+            await _db.SaveChangesAsync();
+            return gift;
         }
     }
 }
