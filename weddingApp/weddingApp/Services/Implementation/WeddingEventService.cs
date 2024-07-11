295786cbe76e13e3e422ea41b3e50fb1f8d13b06
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2024-07-11 22:55:40 +0200

    WeddingEventService

--- weddingApp/weddingApp/Services/Implementation/WeddingEventService.cs
+++ weddingApp/weddingApp/Services/Implementation/WeddingEventService.cs
@@ -20,5 +20,116 @@ namespace weddingApp.Services.Implementation
             var weddingEvents = _db.WeddingEvents.ToListAsync();
             return await weddingEvents;
         }
+        public async Task<WeddingEvent> GetWeddingEventById(int id)
+        {
+            Task<WeddingEvent?>? weddingEvent =  _db.WeddingEvents.FirstOrDefaultAsync(x=>x.Id == id);
+            return await weddingEvent;
+        }
+        public async Task<WeddingEvent> CreateWeddingEvent(WeddingEvent weddingEvent)
+        {
+            _db.WeddingEvents.Add(weddingEvent);
+            await _db.SaveChangesAsync();
+            return weddingEvent;
+        }
+
+        public async Task<WeddingEvent> UpdateWeddingEvent(WeddingEvent weddingEvent)
+        {
+            WeddingEvent? existingWeddingEvent = await _db.WeddingEvents.FindAsync(weddingEvent.Id);
+            if (existingWeddingEvent == null)
+                throw new ArgumentException($"Wedding event with id {weddingEvent.Id} not found");
+            
+            existingWeddingEvent.Couple = weddingEvent.Couple;
+            existingWeddingEvent.WeddingDate = weddingEvent.WeddingDate;
+            existingWeddingEvent.Budget = weddingEvent.Budget;
+            existingWeddingEvent.Name = weddingEvent.Name;
+            existingWeddingEvent.WeddingDate = weddingEvent.WeddingDate;
+            _db.Update(existingWeddingEvent);
+            await _db.SaveChangesAsync();
+            return weddingEvent;
+        }
+        public async Task<WeddingEvent> DeleteWeddingEvent(WeddingEvent weddingEvent)
+        {
+            _db.WeddingEvents.Remove(weddingEvent);
+            await _db.SaveChangesAsync();
+            return weddingEvent;
+        }
+
+        public async Task AddGift(int idWeddingEvent, int idGift)
+        {
+            WeddingEvent? existingWeddingEvent = await _db.WeddingEvents.FindAsync(idWeddingEvent);
+            if (existingWeddingEvent == null)
+                throw new ArgumentException($"Wedding event with id {idWeddingEvent} not found");
+            Gift? existingGift = await _db.Gifts.FindAsync(idGift);
+            if (existingGift == null)
+                throw new ArgumentException($"Gift with id {idGift} not found");
+            existingWeddingEvent.Gifts.Add(existingGift);
+            _db.Update(existingWeddingEvent);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task RemoveGift(int idWeddingEvent, int idGift)
+        {
+            WeddingEvent? existingWeddingEvent = await _db.WeddingEvents.FindAsync(idWeddingEvent);
+            if (existingWeddingEvent == null)
+                throw new ArgumentException($"Wedding event with id {idWeddingEvent} not found");
+            Gift? existingGift = await _db.Gifts.FindAsync(idGift);
+            if (existingGift == null)
+                throw new ArgumentException($"Gift with id {idGift} not found");
+            existingWeddingEvent.Gifts.Remove(existingGift);
+            _db.Update(existingWeddingEvent);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task AddGuest(int idWeddingEvent, int idGuest)
+        {
+            WeddingEvent? existingWeddingEvent = await _db.WeddingEvents.FindAsync(idWeddingEvent);
+            if (existingWeddingEvent == null)
+                throw new ArgumentException($"Wedding event with id {idWeddingEvent} not found");
+            Guest? existingGuest = await _db.Guests.FindAsync(idGuest);
+            if (existingGuest == null)
+                throw new ArgumentException($"Guest with id {idGuest} not found");
+            existingWeddingEvent.Guests.Add(existingGuest);
+            _db.Update(existingWeddingEvent);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task RemoveGuest(int idWeddingEvent, int idGuest)
+        {
+            WeddingEvent? existingWeddingEvent = await _db.WeddingEvents.FindAsync(idWeddingEvent);
+            if (existingWeddingEvent == null)
+                throw new ArgumentException($"Wedding event with id {idWeddingEvent} not found");
+            Guest? existingGuest = await _db.Guests.FindAsync(idGuest);
+            if (existingGuest == null)
+                throw new ArgumentException($"Guest with id {idGuest} not found");
+            existingWeddingEvent.Guests.Remove(existingGuest);
+            _db.Update(existingWeddingEvent);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task AddWeddingService(int idWeddingEvent, int idWeddingService)
+        {
+            WeddingEvent? existingWeddingEvent = await _db.WeddingEvents.FindAsync(idWeddingEvent);
+            if (existingWeddingEvent == null)
+                throw new ArgumentException($"Wedding event with id {idWeddingEvent} not found");
+            WeddingService? existingWeddingService = await _db.WeddingServices.FindAsync(idWeddingService);
+            if (existingWeddingService == null)
+                throw new ArgumentException($"Wedding Service with id {idWeddingService} not found");
+            existingWeddingEvent.WeddingServices.Add(existingWeddingService);
+            _db.Update(existingWeddingEvent);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task RemoveWeddingService(int idWeddingEvent, int idWeddingService)
+        {
+            WeddingEvent? existingWeddingEvent = await _db.WeddingEvents.FindAsync(idWeddingEvent);
+            if (existingWeddingEvent == null)
+                throw new ArgumentException($"Wedding event with id {idWeddingEvent} not found");
+            WeddingService? existingWeddingService = await _db.WeddingServices.FindAsync(idWeddingService);
+            if (existingWeddingService == null)
+                throw new ArgumentException($"Wedding Service with id {idWeddingService} not found");
+            existingWeddingEvent.WeddingServices.Remove(existingWeddingService);
+            _db.Update(existingWeddingEvent);
+            await _db.SaveChangesAsync();
+        }
     }
 }
--- weddingApp/weddingApp/Services/Interfaces/IWeddingEventService.cs
+++ weddingApp/weddingApp/Services/Interfaces/IWeddingEventService.cs
@@ -5,5 +5,15 @@ namespace weddingApp.Services.Interfaces
     public interface IWeddingEventService
     {
         Task<IEnumerable<WeddingEvent>> GetAllWeddingEvents();
+        Task<WeddingEvent> GetWeddingEventById(int id );
+        Task<WeddingEvent> CreateWeddingEvent(WeddingEvent weddingEvent);
+        Task<WeddingEvent> UpdateWeddingEvent(WeddingEvent weddingEvent);
+        Task<WeddingEvent> DeleteWeddingEvent(WeddingEvent weddingEvent);
+        Task AddGift(int idWeddingEvent, int idGift);
+        Task RemoveGift(int idWeddingEvent, int idGift);
+        Task AddGuest(int idWeddingEvent, int idGuest);
+        Task RemoveGuest(int idWeddingEvent, int idGuest);
+        Task AddWeddingService(int idWeddingEvent, int idWeddingService);
+        Task RemoveWeddingService(int idWeddingEvent, int idWeddingService);
     }
 }
